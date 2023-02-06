e96bb3f03aa7f7ccdb778a5046c0251b0a35f27c
NdekoCode <arickbulakali@gmail.com>
2023-02-06 21:06:55 +0200

    feat: add controller in routes for product

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import ProductsController from "../controllers/ProductsController.js";
 
 const productRouter = Router();
-productRouter.get("/getAll");
-productRouter.get("/getOne");
-productRouter.post("/createOne");
-productRouter.put("/updateOne");
-productRouter.delete("/deleteOne");
+const productsCTRL = new ProductsController();
+productRouter.get("/", productsCTRL.getAll);
+productRouter.get("/:id", productsCTRL.getOne);
+productRouter.post("/add", productsCTRL.createOne);
+productRouter.put("/update/:id", productsCTRL.updateOne);
+productRouter.delete("/delete/:id", productsCTRL.deleteOne);
 export default productRouter;
