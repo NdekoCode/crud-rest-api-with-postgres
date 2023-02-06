ee82c0cdaa9a8f00f72a4abf9cc70b1beecf967e
NdekoCode <arickbulakali@gmail.com>
2023-02-06 22:38:58 +0200

    refactor: db connection

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,18 @@
 import { Sequelize } from "sequelize";
+
+const logging = () => (process.env.NODE_ENV === "development" ? true : false);
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+const DB_DIALECT = process.env.DB_DIALECT;
+console.log(DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT);
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  dialect: DB_DIALECT,
+  host: DB_HOST,
+  logging,
+});
 export default async function connectDatabase(app) {
-  const DB_URL = process.env.DB_URL;
-  const logging = () =>
-    process.env.NODE_ENV === "developement" ? true : false;
-  const sequelize = new Sequelize(DB_URL, { logging });
   const PORT = process.env.PORT || 4500;
   try {
     await sequelize.sync();
