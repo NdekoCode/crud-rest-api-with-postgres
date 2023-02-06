740dd6f88d3c4d8e803308207f14ed06914bb58a
NdekoCode <arickbulakali@gmail.com>
2023-02-06 21:04:40 +0200

    feat: add default controller functions

---
+++ controllers/ProductsController.js
@@ -0,0 +1,7 @@
+export default class ProductsController {
+  async getAll(req, res) {}
+  async getOne(req, res) {}
+  async createOne(req, res) {}
+  async updateOne(req, res) {}
+  async deleteOne(req, res) {}
+}
