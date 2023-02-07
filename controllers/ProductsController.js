625cbd8e05716b6f00c8704581d24850e648a41f
NdekoCode <arickbulakali@gmail.com>
2023-02-07 18:22:48 +0200

    feat: add product

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -11,6 +11,16 @@ export default class ProductsController {
     if (error) {
       return res.status(400).json({ error: error.details[0].message });
     }
+    const product = await Product.findOne({
+      where: { title: bodyRequest.title },
+    });
+    if (product) {
+      return res.status(409).json({ error: "Le produit existe déjà" });
+    }
+    await Product.create(bodyRequest);
+    return res
+      .status(201)
+      .json({ error: null, message: "Produit ajouter avec succés" });
   }
   async updateOne(req, res) {}
   async deleteOne(req, res) {}
--- libs/validations.js
+++ libs/validations.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const addProductValidation = (data) => {
   const Schema = Joi.object({
     title: Joi.string().min(3).max(255).required(),
-    description: Joi.string().min(10),
+    description: Joi.string().min(10).max(500),
     price: Joi.number().min(0).required(),
   });
   return Schema.validate(data);
