725c8085c5e6faa9a68cb68cbb01a2d420b720f1
NdekoCode <arickbulakali@gmail.com>
2023-02-07 18:14:57 +0200

    feat: addProduct validation

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,10 +1,17 @@
+import { addProductValidation } from "../libs/validations.js";
 import Product from "../models/Product.js";
 export default class ProductsController {
   async getAll(req, res) {}
   async getOne(req, res) {
     console.log(Product);
   }
-  async createOne(req, res) {}
+  async createOne(req, res) {
+    const bodyRequest = req.body;
+    const { error } = addProductValidation(bodyRequest);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+  }
   async updateOne(req, res) {}
   async deleteOne(req, res) {}
 }
---
+++ libs/validations.js
@@ -0,0 +1,10 @@
+import Joi from "joi";
+
+export const addProductValidation = (data) => {
+  const Schema = Joi.object({
+    title: Joi.string().min(3).max(255).required(),
+    description: Joi.string().min(10),
+    price: Joi.number().min(0).required(),
+  });
+  return Schema.validate(data);
+};
