fe5ecb55b1d7c24824f40d03a17717cad9103924
NdekoCode <arickbulakali@gmail.com>
2023-02-07 18:54:18 +0200

    refactor: capture errors in crud operations

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -2,21 +2,29 @@ import { addProductValidation } from "../libs/validations.js";
 import Product from "../models/Product.js";
 export default class ProductsController {
   async getAll(req, res) {
-    // On recupere tout saut le champ updatedAt et createdAt
-    const products = await Product.findAll({
-      attributes: { exclude: ["createdAt", "updatedAt"] },
-    });
-    return res.status(200).json(products || []);
+    try {
+      // On recupere tout saut le champ updatedAt et createdAt
+      const products = await Product.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+      });
+      return res.status(200).json(products || []);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
   async getOne(req, res) {
     const id = req.params.id;
-    const product = await Product.findByPk(id, {
-      attributes: { exclude: ["updatedAt"] },
-    });
-    if (product) {
-      return res.status(200).json(product);
+    try {
+      const product = await Product.findByPk(id, {
+        attributes: { exclude: ["updatedAt"] },
+      });
+      if (product) {
+        return res.status(200).json(product);
+      }
+      return res.status(404).json({ error: "Le produit est introuvable" });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
-    return res.status(404).json({ error: "Le produit est introuvable" });
   }
   async createOne(req, res) {
     const bodyRequest = req.body;
@@ -24,16 +32,20 @@ export default class ProductsController {
     if (error) {
       return res.status(400).json({ error: error.details[0].message });
     }
-    const product = await Product.findOne({
-      where: { title: bodyRequest.title },
-    });
-    if (product) {
-      return res.status(409).json({ error: "Le produit existe déjà" });
+    try {
+      const product = await Product.findOne({
+        where: { title: bodyRequest.title },
+      });
+      if (product) {
+        return res.status(409).json({ error: "Le produit existe déjà" });
+      }
+      await Product.create(bodyRequest);
+      return res
+        .status(201)
+        .json({ error: null, message: "Produit ajouter avec succés" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-    await Product.create(bodyRequest);
-    return res
-      .status(201)
-      .json({ error: null, message: "Produit ajouter avec succés" });
   }
   async updateOne(req, res) {}
   async deleteOne(req, res) {}
